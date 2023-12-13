d5ec70ca2e622468cd4ea851ce4eeb22123dc479
Oussama El Mouekken <oel-moue@c4r10p9.1337.ma>
2023-12-13 23:02:23 +0100

    test

---
+++ main.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oel-moue <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/13 16:11:24 by oel-moue          #+#    #+#             */
+/*   Updated: 2023/12/13 16:11:35 by oel-moue         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+// #include <stdio.h>
+// #include <unistd.h>
+// #include <fcntl.h>
+// #include <stdlib.h>
+
+// int f(int nb)
+// {
+//     static int s = 0;
+//     s += nb;
+//     return s;
+// }
+
+// int main () 
+// {
+//     int f,p;
+//     char  buffer[8];
+
+//     f = open("abc1.txt", O_WRONLY | O_CREAT);
+//     write(f, "oussama hello",13); 
+
+//     close(f);
+
+//     f = open("abc1.txt", O_RDONLY);
+//     p = read(f, buffer, 4); 
+
+//     close(f);
+ 
+//     printf("%s\t%d",buffer,p);
+//     printf("\n");
+//     printf("%s\t%d",buffer,p);
+// }
+
